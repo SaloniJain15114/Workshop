f72683222a7528bd215581e5e8f0be4392d1da41
SaloniJain15114 <salonijain15114@gmail.com>
2025-10-14 21:16:54 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,68 @@
+/*1st Question-*/
+/*const obj={
+    name:"alice",
+    details:{
+        age:25,
+        city:"paris"
+    }
+}
+console.log(obj.details['city'],obj.details['age'])
+
+/* 2nd question-*/
+/*const obj1={a:1,b:2};
+const obj2={b:3,c:4};
+const merged={...obj1,...obj2};
+console.log(merged);
+
+/* 3rd question- */
+/*const product={id:1, name:"Laptop",price:1000}
+const productArr=Object.entries(product)
+console.log(productArr)
+
+/* 4th question- */
+/*const obj={
+    name:"alice",
+    details:{
+        age:25,
+        city:"paris"
+    }
+}
+const updateobj={
+    name:"alice",
+    details:{
+        age:25,
+        city:"toyko"
+    }
+}
+console.log(obj.details['city'])
+console.log(updateobj.details['city'])*/
+
+/* 5th question- */
+/*const score={alice:85,bob:58,charlie:90,dave:45}
+ const filterscore = score.filter((el,index) => el>60)
+ console.log(filterscore)*/
+
+ /* 6th queation
+ const Arr=[1,2,2,3,4,4,5]
+ const uniqueArr=[...new Set(Arr)]
+ console.log(uniqueArr)*/
+
+ /* 7th question*/
+ /*const arr=[1,[2,[3,[4,5]]]]
+ const flatArr=arr.flat(Infinity)
+ console.log(flatArr)*/
+
+ /* 8rd question */
+ /*const user=[
+    { name: "John", age:30},
+    { name:"Alice", age:25},
+    { name: "Bob",age:28}
+ ]
+ const sortUser=user.sort((a,b) => a.age - b.age)
+ console.log(sortUser)*/
+
+
+
+
+
+
